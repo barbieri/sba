3e2474bbbea4b5144f4fbca70d6734a6a6d89da3
Gustavo Sverzut Barbieri <barbieri@profusion.mobi>
2011-05-30 01:18:49 -0300

    Add sales module.

---
+++ sales/admin.py
@@ -0,0 +1,107 @@
+from sales.models import CustomerGeoState, CustomerCity, Customer, \
+    Product, RevenueMethod, Sale, SaleProduct, SaleRevenue
+from django.contrib import admin
+from django.db.models import Sum
+from django import forms
+
+
+class CustomerCityInline(admin.TabularInline):
+    model = CustomerCity
+    extra = 3
+
+
+class CustomerCityAdmin(admin.ModelAdmin):
+    list_display = ["name", "state"]
+    list_filter = ["state"]
+    search_fields = ["name", "state"]
+
+
+class CustomerGeoStateAdmin(admin.ModelAdmin):
+    inlines = [CustomerCityInline]
+    list_display = ["code", "name"]
+    list_filter = ["code"]
+    search_fields = ["code", "name"]
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["identifier", "description", "active", "cost"]
+    list_filter = ["active"]
+    search_fields = ["identifier", "description"]
+    actions = ["make_inactive"]
+
+    def make_inactive(self, request, queryset):
+        rows_updated = queryset.update(active=False)
+        if rows_updated < 1:
+            message_bit = "No product"
+        elif rows_updated == 1:
+            message_bit = "1 product"
+        else:
+            message_bit = "%d products" % rows_updated
+        self.message_user(request, "%s marked as inactive" % message_bit)
+    make_inactive.short_description = "Mark selected as inactive"
+
+
+class SaleRevenueInline(admin.TabularInline):
+    model = SaleRevenue
+    readonly_fields = ["operation_cost", "percentual_cost", "net_value"]
+    extra = 3
+
+
+class SaleProductInline(admin.TabularInline):
+    model = SaleProduct
+    extra = 5
+
+
+class SaleAdmin(admin.ModelAdmin):
+    inlines = [SaleRevenueInline, SaleProductInline]
+    list_display = ["datetime", "vendor", "customer", "value",
+                    "discount", "product_count", "revenue_count"]
+    list_filter = ["datetime", "vendor", "customer", "discount"]
+    search_fields = ["vendor", "customer"]
+    date_hierarchy = "datetime"
+    actions = ["summary"]
+    readonly_fields = ["value"]
+
+    def product_count(self, o):
+        v = o.saleproduct_set.aggregate(Sum("count")).get("count__sum")
+        if v:
+            return v
+        return ""
+    product_count.short_description = "Products"
+
+    def revenue_count(self, o):
+        return o.salerevenue_set.count()
+    revenue_count.short_description = "Revenues"
+
+    def summary(self, request, queryset):
+        message = ""
+
+        count = queryset.count()
+        if count < 1:
+            message = "No sales selected"
+        else:
+            revenue = 0.0
+            discount = 0.0
+            products = 0
+            for o in queryset.all():
+                revenue += o.value
+                discount += o.discount
+                products += o.saleproduct_set.aggregate(Sum("count")
+                                                        ).get("count__sum")
+            message = ("%d sales, %d products (%0.1f average), "
+                       "%0.0f%% average discount, "
+                       "revenue %0.2f (%0.2f average)") % \
+                       (count, products, products / float(count),
+                        discount / count,
+                        revenue, revenue / count)
+
+        self.message_user(request, message)
+    summary.short_description = "Summary"
+
+
+admin.site.register(CustomerGeoState, CustomerGeoStateAdmin)
+admin.site.register(CustomerCity, CustomerCityAdmin)
+admin.site.register(Customer)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Sale, SaleAdmin)
+admin.site.register(RevenueMethod)
---
+++ sales/models.py
@@ -0,0 +1,137 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
+from django.db.models import Sum
+import datetime
+
+
+def today():
+    v = datetime.date.today()
+    return datetime.datetime(v.year, v.month, v.day)
+
+
+class CustomerGeoState(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=2)
+
+    def __unicode__(self):
+        return u"%s - %s" % (self.code, self.name)
+
+    class Meta:
+        ordering = ["code"]
+        verbose_name = "Customer State"
+
+
+class CustomerCity(models.Model):
+    name = models.CharField(max_length=50)
+    state = models.ForeignKey(CustomerGeoState)
+
+    def __unicode__(self):
+        return u"%s-%s" % (self.name, self.state.code)
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(blank=True)
+    address = models.CharField(max_length=50, blank=True)
+    neighborhood = models.CharField(max_length=30, blank=True)
+    city = models.ForeignKey(CustomerCity, blank=True)
+    zip = models.CharField(max_length=9, blank=True)
+    note = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Product(models.Model):
+    identifier = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    note = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
+    cost = models.FloatField()
+
+    def __unicode__(self):
+        return self.identifier
+
+
+class RevenueMethod(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    operation_cost = models.FloatField(default=0.0)
+    percentual_cost = models.FloatField(default=0.0)
+    active = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Sale(models.Model):
+    datetime = models.DateTimeField(default=today)
+    vendor = models.ForeignKey(User, blank=True, null=True)
+    customer = models.ForeignKey(Customer, blank=True, null=True)
+    value = models.FloatField(default=0.0,
+                              help_text="Updated automatically on save.")
+    discount = models.FloatField(default=0.0)
+
+    def recalc(self):
+        v = self.salerevenue_set.aggregate(Sum("value")).get("value__sum")
+        if v is None:
+            v = 0.0
+        self.value = v
+        self.save()
+
+    def __unicode__(self):
+        if self.vendor:
+            return u"%s: %s (%s)" % (self.datetime.strftime("%Y-%m-%d %H:%M"),
+                                     self.value, self.vendor)
+        else:
+            return u"%s: %s" % (self.datetime.strftime("%Y-%m-%d %H:%M"),
+                                self.value)
+
+    class Meta:
+        ordering = ["datetime"]
+
+
+class SaleProduct(models.Model):
+    sale = models.ForeignKey(Sale)
+    product = models.ForeignKey(Product, limit_choices_to={"active": True})
+    count = models.IntegerField(validators=[MinValueValidator(1)])
+
+
+class SaleRevenue(models.Model):
+    sale = models.ForeignKey(Sale)
+    date_due = models.DateField(default=datetime.date.today)
+    value = models.FloatField(validators=[MinValueValidator(1.0)])
+
+    # method is referenced, but operation_cost and percentual_cost are
+    # copied from RevenueMethod at the moment of the sale. Values
+    # should not change when the reference changes.
+    method = models.ForeignKey(RevenueMethod, limit_choices_to={"active": True})
+    operation_cost = models.FloatField(default=0)
+    percentual_cost = models.FloatField(default=0)
+    net_value = models.FloatField(default=0)
+
+    def __unicode__(self):
+        return u"%s: %s (%s)" % (self.date_due, self.value, self.method)
+
+    def save(self):
+        self.operation_cost = self.method.operation_cost
+        self.percentual_cost = self.method.percentual_cost
+        self.net_value = (self.value * ((100.0 - self.percentual_cost) / 100.0)
+                          - self.operation_cost)
+        super(SaleRevenue, self).save()
+        self.sale.recalc()
+
+    def delete(self):
+        sale = self.sale
+        super(SaleRevenue, self).delete()
+        sale.recalc()
